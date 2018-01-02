73fa3fe688ee54c482cd643cde46437bd308d611
swift <swiftsierra@swifts-iMac.local>
2018-01-01 19:01:37 -0500

    Create Announcements TableView

--- MSE Utilities/MSE Utilities/Announcements.swift
+++ MSE Utilities/MSE Utilities/Announcements.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
-class Announcements: UIViewController {
+class Announcements: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        var myTableView = UITableView(frame: self.view.bounds, style: UITableViewStyle.plain)
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        
+        myTableView.backgroundColor = UIColor.white
+        myTableView.frame = CGRect(x: 20, y: 50, width: 250, height: 400)
+        
+        myTableView.register(UINib(nibName: "TestCell", bundle: nil), forCellReuseIdentifier: "myIdentifier")
+        
+        self.view.addSubview(myTableView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +30,15 @@ class Announcements: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "myIdentifier") as! TestCell
+        myCell.labelOne.text = "Row \(indexPath.row)"
+        return myCell
+    }
 
     /*
     // MARK: - Navigation
---
+++ MSE Utilities/MSE Utilities/TestCell.swift
@@ -0,0 +1,25 @@
+//
+//  TestCell.swift
+//  MSE Utilities
+//
+//  Created by swift on 1/1/18.
+//  Copyright © 2018 HSMSE. All rights reserved.
+//
+
+import UIKit
+
+class TestCell: UITableViewCell {
+
+    @IBOutlet weak var labelOne: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
