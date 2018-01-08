6005df56738451fac40e0b33bd5d619fb2cf0a33
Temp <Temp@ad25md038f8j5.78m692.nycboe.org>
2018-01-08 13:46:51 -0500

    Add base node server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/views/index.html'))
+})
+
+app.listen(3000)
