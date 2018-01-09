c2bfcce9fc6018790e779f72e5fcd5e18bbb7273
Benjamin Kosten <benkosten@gmail.com>
2018-01-09 14:59:05 -0500

    Fix server git stuff

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.use('/static', express.static(__dirname + '/static'));
+
+//Announcement View
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/views/index.html'))
+})
+
+//Api Routing
+
+// Starts the server.
+server.listen(3000, function() {
+  console.log('Starting server on port 3000');
+});
