b5098ed8e66107e0caca6c5b9784b92f06cc4041
Benjamin Kosten <benkosten@gmail.com>
2018-01-09 15:35:28 -0500

    Base website added (Now with nodejs)

--- server/server.js
+++ server/server.js
@@ -2,16 +2,19 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
-app.use('/static', express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/static'));
 
 //Announcement View
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/views/index.html'))
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname + '/static/index.html'))
 })
 
 //Api Routing
+app.get('/api/announcements', function(req, res) {
+    res.status(200).json('{timestamp: stuff}')
+})
 
 // Starts the server.
 app.listen(3000, function() {
-  console.log('Starting server on port 3000');
+    console.log('Starting server on port 3000');
 });
