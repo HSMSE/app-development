84d4adc411fc25d1b7b9dae75532126f77ac27de
bendgk <benkosten@gmail.com>
2018-01-09 22:03:15 -0500

    Add verification of google token id

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
-var express = require("express");
-var app = express();
-var path = require("path");
+const express = require("express")
+const app = express()
+const path = require("path")
+const bodyParser = require('body-parser')
 
-app.use(express.static(__dirname + '/static'));
+const keys = require('./keys.json');
+var verifier = require('google-id-token-verifier');
+
+app.use(express.static(__dirname + '/static'))
+app.use(bodyParser.json())
 
 //Announcement View
 app.get('/',function(req,res) {
     res.sendFile(path.join(__dirname + '/static/index.html'))
+})
+
+//handle signin
+app.post('/g-signin', function(req, res) {
+    var g_token = req.body['token']
+    verifier.verify(g_token, keys['web']['client_id'], function (err, tokenInfo) {
+        if (!err) {
+            // use tokenInfo in here.
+            console.log(tokenInfo)
+        }
+    })
+
 })
 
 //Api Routing
@@ -16,5 +33,5 @@ app.get('/api/announcements', function(req, res) {
 
 // Starts the server.
 app.listen(3000, function() {
-    console.log('Starting server on port 3000');
+    console.log('Starting server on port 3000')
 });
