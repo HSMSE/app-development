cc09acc90bcc50d20fe2a4d0dc52ea3513b32ec2
Temp <Temp@ad25nj07zf8j5.78m692.nycboe.org>
2018-03-26 13:53:03 -0400

    Allow date to change color when selected

--- MSE Utilities/MSE Utilities/CalendarViewController.swift
+++ MSE Utilities/MSE Utilities/CalendarViewController.swift
@@ -95,10 +95,12 @@ extension CalendarViewController: JTAppleCalendarViewDelegate {
     
     func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
         handleCellSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
     }
     
     func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
         handleCellSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
     }
 }
 
