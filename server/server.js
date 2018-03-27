98447fadd5cf9cbf0e5a4ac2d98972c7a53cf82a
bendgk <benkosten@gmail.com>
2018-03-26 21:45:03 -0400

    Uhh I've lost track of things a while ago...

--- server/server.js
+++ server/server.js
@@ -3,18 +3,10 @@ const app = express()
 const path = require("path")
 const bodyParser = require('body-parser')
 
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-const adapter = new FileSync('announcements.json')
-const db = low(adapter)
-
 const keys = require('./keys.json')
 const verifier = require('google-id-token-verifier')
 
 const utils = require('./utils/utils.js')
-
-//db setup
-db.defaults({announcements: []}).write()
 
 //express setup
 app.use(express.static(__dirname + '/static'))
@@ -37,42 +29,18 @@ app.get('/client_page', function(req, res) {
     res.sendFile(__dirname + '/views/client_page.html')
 })
 
-app.get('/test', function(req, res) {
-    db.get('announcements')
-        .push({date: req.query["date"], subject: req.query["subject"], message: req.query["message"]})
-        .write()
-
-    res.json(req.query)
+app.post('/api/announcements', function(req, res) {
+    utils.postAnnouncements(req.body)
+    res.redirect('/client_page')
 })
 
 app.get('/api/announcements', function(req, res) {
-    utils.getAnnouncements(function(err, fileData) {
-        res.json(fileData)
-    })
-})
-
-/*
-Announcement Routing
-Announcemnts will be in the format:
-{"2012-04-23": [{"m": "Seniors pay prom deposits", "t": "Boylan", "s": "Prom"}, {"m": "Freshman don't be loud", "t": "Zara", "s": "Noise Complaint"}]}
-
-m -> message
-t -> teacher
-s -> subject
-d -> date
-
-date format will be compliant with: https://stackoverflow.com/questions/10286204/the-right-json-date-format
-yyyy-mm-dd
-
-app.post('/api/announcements', function(req, res) {
-
-    utils.postAnnouncements(req.body, function(err) {
+    utils.getAnnouncements(function(err, data) {
         if(!err) {
-            res.send(true)
+            res.json(data)
         }
     })
 })
-*/
 
 // Starts the server.
 app.listen(3000, function() {
--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,25 +1,31 @@
 const PythonShell = require('python-shell')
-const jsonfile = require('jsonfile');
 
-//Utility file for various scripts
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('announcements.json')
+const db = low(adapter)
+
+//db setup
+db.defaults({announcements: []}).write()
+
+//utility file for various scripts
 module.exports = {
     getAuthorizedEmails: function(callback) {
         PythonShell.run('utils/emails.py', callback)
     },
 
-    getAnnouncements: function(callback) {
-        jsonfile.readFile('announcements.json', callback)
-    }
+    getAnnouncements: function(amt, callback) {
+        callback(null,
+            db.get('announcements')
+                .orderBy("date", 'desc')
+                .take(100)
+                .value()
+        )
+    },
 
-    //something like this, but this is completely wrong becuase it doesnt unpack the array of announcements or events I call them I guess something like that
-    /*
-    postAnnouncements: function(body, callback) {
-        message = body['m']
-        teacher = body['t']
-        subject = body['s']
-        date = body['d']
-
-        jsonfile.writeFile('announcemnts.json', {d: }, {flag: 'a'}, callback)
+    postAnnouncements: function(data) {
+        db.get('announcements')
+            .push({date: data["date"], subject: data["subject"], message: data["message"]})
+            .write()
     }
-    */
 }
