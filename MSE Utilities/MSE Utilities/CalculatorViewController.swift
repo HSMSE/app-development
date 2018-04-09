0175becd4716a493e5467ca7ef0ab3af79fcb9dd
Benjamin Kosten <benkosten@gmail.com>
2018-04-09 11:54:51 -0400

    Add demos web view

--- MSE Utilities/MSE Utilities/CalculatorViewController.swift
+++ MSE Utilities/MSE Utilities/CalculatorViewController.swift
@@ -10,16 +10,25 @@ import Foundation
 import UIKit
 
 class CalculatorViewController: UIViewController {
-  
+    
+
+    @IBOutlet weak var WebView: UIWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // Do any additional setup after loading the view.
+        
+        let htmlFile = Bundle.main.path(forResource: "desmos", ofType: "html")
+        let html = try? String(contentsOfFile: htmlFile!, encoding: String.Encoding.utf8)
+        WebView.loadHTMLString(html!, baseURL: nil)
+        
+        //WebView.loadRequest(NSURLRequest(url: NSURL(fileURLWithPath:Bundle.main.path(forResource: "desmos.html", ofType: "html")!) as URL) as URLRequest)
     }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
+}
+
+
 }
