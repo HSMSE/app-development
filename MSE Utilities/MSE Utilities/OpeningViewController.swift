8c6e176502bfb35d030540409f339eec9f79b16c
Benjamin Kosten <benkosten@gmail.com>
2018-04-18 15:23:05 -0400

    add dashboard

---
+++ MSE Utilities/MSE Utilities/OpeningViewController.swift
@@ -0,0 +1,24 @@
+//
+//  OpeningViewController.swift
+//  MSE Utilities
+//
+//  Created by Temp on 4/17/18.
+//  Copyright © 2018 HSMSE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OpeningViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
